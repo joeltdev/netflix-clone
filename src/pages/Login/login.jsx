2fc66f44357a79ef152952ce072ff0bad2ce2192
joeltdev <113245682+joeltdev@users.noreply.github.com>
2024-12-12 01:59:02 +0530

    added the signup

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -3,12 +3,20 @@ import "./login.css";
 import logo from "../../assets/logo.png";
 import { login, signup } from "../../firebase";
 
-const login = () => {
+const Login = () => {
   const [signState, setSignState] = useState("Sign In");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const user_auth = async (e) => {
+    e.preventDefault();
+    if (signState === "Sign In") {
+      await login(email, password);
+    } else {
+      await signup(name, email, password);
+    }
+  };
   return (
     <div className="login">
       <img src={logo} alt="" className="login-logo" />
@@ -16,13 +24,36 @@ const login = () => {
         <h1>{signState}</h1>
         <form action="">
           {signState === "Sign Up" ? (
-            <input type="text" placeholder="Your name" />
+            <input
+              value={name}
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+              type="text"
+              placeholder="Your name"
+            />
           ) : (
             <></>
           )}
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="password" />
-          <button>{signState}</button>
+          <input
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            value={password}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            type="password"
+            placeholder="password"
+          />
+          <button onClick={user_auth} type="submit">
+            {signState}
+          </button>
           <div className="form-help">
             <div className="remember">
               <input type="checkbox" />
@@ -61,4 +92,4 @@ const login = () => {
   );
 };
 
-export default login;
+export default Login;
